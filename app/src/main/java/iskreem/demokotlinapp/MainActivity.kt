5f8bd7899fb76c59c2c7c75876630e3f8c81e096
Fernando Cob <fernandocob@outlook.com>
2018-03-04 10:01:36 -0600

    Initial commit

---
+++ app/src/main/java/iskreem/demokotlinapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package iskreem.demokotlinapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
